04845ecd972ccbd6bcb92b5e83a4293e4e11f97e
권세진 <0307kwon@gmail.com>
2020-12-01 22:54:05 +0900

    feat: 컴포넌스 사용하여 웹페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+
+class Article extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">HTML</a></li>
+            <li><a href="3.html">HTML</a></li>
+        </ul>
+      </nav>    
+    );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web~!
+      </header>
+    );
+
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!~~
+        <Subject></Subject>
+        <Navigation></Navigation>
+        <Article></Article>
       </div>
     );
   }
